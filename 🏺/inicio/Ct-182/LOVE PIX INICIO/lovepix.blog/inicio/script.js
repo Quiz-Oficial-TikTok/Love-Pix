7902008c4ade59b90522ce602e3552502f798104
Iota <163447700+Avaliador-Premiado@users.noreply.github.com>
2024-08-15 11:05:18 -0300

    .

---
+++ 🏺/inicio/Ct-182/LOVE PIX INICIO/lovepix.blog/inicio/script.js
@@ -0,0 +1,45 @@
+// DOM
+const swiper = document.querySelector('#swiper');
+const like = document.querySelector('#like');
+const dislike = document.querySelector('#dislike');
+
+// constants
+const urls = [
+  'https://appsofc.online/modelopixi/001.png',
+  '../../appsofc.online/modelopixi/002.png',
+  '../../appsofc.online/modelopixi/003.png',
+  '../../appsofc.online/modelopixi/004.png',
+  '../../appsofc.online/modelopixi/005.png'
+];
+
+// variables
+let cardCount = 0;
+
+// functions
+function appendNewCard() {
+  const card = new Card({
+    imageUrl: urls[cardCount % 5],
+    onDismiss: appendNewCard,
+    onLike: () => {
+      like.style.animationPlayState = 'running';
+      like.classList.toggle('trigger');
+    },
+    onDislike: () => {
+      dislike.style.animationPlayState = 'running';
+      dislike.classList.toggle('trigger');
+    }
+  });
+  swiper.append(card.element);
+  cardCount++;
+
+  const cards = swiper.querySelectorAll('.card:not(.dismissing)');
+  cards.forEach((card, index) => {
+    card.style.setProperty('--i', index);
+  });
+}
+
+// first 5 cards
+for (let i = 0; i < 5; i++) {
+  appendNewCard();
+}
+
